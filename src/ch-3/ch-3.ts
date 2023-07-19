9e5621c43572c81a19292f96c8cd4b21be27f72d
jlee0505 <hj960322@gmail.com>
2023-07-20 07:51:43 +0900

    ch.3 예제 추가

--- src/ch-3/ch-3.ts
+++ src/ch-3/ch-3.ts
@@ -13,12 +13,12 @@ const enum Color {
 }
 const a = Color.Red; // "#c10000"
 const b1 = Color.White; // 255
-const b2 = Color['White']; // 255
-const b3 = Color[255] // "White", 단, const enum으로 정의한 경우는 Error
-const b4 = Color.255 // Error
+const b2 = Color["White"]; // 255
+// const b3 = Color[255]; // "White", 단, const enum으로 정의한 경우는 Error
+// const b4 = Color.255 // Error
 
-const c = Color[6];
+const c = Color.Green;
 // enum Color
-console.log(c) // undefined
+console.log(c); // undefined
 // const enum Color
-console.log(c) // error TS2476: A const enum member can only be accessed using a string literal.
+console.log(c); // error TS2476: A const enum member can only be accessed using a string literal.
